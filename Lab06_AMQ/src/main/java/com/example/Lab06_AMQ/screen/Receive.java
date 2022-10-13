e0a776df4334042b1dd10f6da93d945bc9eb6b2d
ThaiAn31 <nguyenthaian31102001@gmail.com>
2022-10-13 22:45:49 +0700

    Second commit

---
+++ Lab06_AMQ/src/main/java/com/example/Lab06_AMQ/screen/Receive.java
@@ -0,0 +1,159 @@
+package com.example.Lab06_AMQ.screen;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.apache.log4j.BasicConfigurator;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class Receive extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Receive frame = new Receive();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Receive() {
+		setTitle("Chat");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(30, 234, 267, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.setBounds(322, 233, 85, 21);
+		contentPane.add(btnSend);
+		
+		JButton btnNewButton_1 = new JButton("Reset");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(322, 195, 85, 21);
+		contentPane.add(btnNewButton_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(30, 29, 267, 188);
+		contentPane.add(textArea);
+		try {
+			callSub();
+		} catch (Exception e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String ms = contentPane.getToolTipText().toString();
+				try {
+//					callSub();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				contentPane.setToolTipText(ms);
+
+			}
+		});
+	}
+	public void callSub() throws Exception {
+
+		BasicConfigurator.configure();
+
+		Properties settings = new Properties();
+		settings.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+		settings.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+
+		Context ctx = new InitialContext(settings);
+
+		Object obj = ctx.lookup("ConnectionFactory");
+		ConnectionFactory factory = (ConnectionFactory) obj;
+
+		Destination destination = (Destination) ctx.lookup("dynamicQueues/thanthidet");
+
+		Connection con = factory.createConnection("admin", "admin");
+
+		con.start();
+
+		Session session = con.createSession(/* transaction */false, /* ACK */Session.CLIENT_ACKNOWLEDGE);
+
+		MessageConsumer receiver = session.createConsumer(destination);
+
+		System.out.println("Tý was listened on queue...");
+		receiver.setMessageListener(new MessageListener() {
+
+
+			public void onMessage(Message msg) {
+				try {
+					if (msg instanceof TextMessage) {
+						TextMessage tm = (TextMessage) msg;
+						String txt = tm.getText();
+						suLyXML(msg);
+						contentPane.setToolTipText("Nhận được " + txt);
+						msg.acknowledge();
+					} else if (msg instanceof ObjectMessage) {
+						ObjectMessage om = (ObjectMessage) msg;
+						System.out.println(om);
+					}
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			private void suLyXML(Message msg) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+}
